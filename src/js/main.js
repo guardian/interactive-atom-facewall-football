1a8cc4700fe70ba9001bd94aca96b9ad33ce0488
Garry Blight <garry.blight@guardian.co.uk>
2017-09-13 11:26:18 +0100

    first commit

---
+++ src/js/main.js
@@ -0,0 +1,8 @@
+var el = document.createElement('script');
+el.src = '<%= path %>/app.js';
+document.body.appendChild(el);
+
+
+
+//var floor = document.getElementById("level-1");
+
---
+++ src/render.js
@@ -0,0 +1,45 @@
+import Handlebars from 'handlebars/dist/handlebars'
+import rp from 'request-promise'
+import mainTemplate from './src/templates/main.html!text'
+
+
+Handlebars.registerHelper("ifvalue", function(conditional, options) {
+    if (conditional == options.hash.equals) {
+        return options.fn(this);
+    } else {
+        return options.inverse(this);
+    }
+});
+
+
+export function render() {
+    return rp({
+        uri: 'https://interactive.guim.co.uk/docsdata/1e0h3eAZRkXJCrh5jN7TIRkp-qkMBa1WjihzdOalpOYE.json',
+        json: true
+    }).then((data) => {
+        var sheets = data.sheets;        
+        var maxSteps = sheets.Floors.length;
+
+        sheets.Floors.map((obj,k)=>{
+            obj.maxSteps = maxSteps; //aded maxSteps ref for app.js
+
+            if(obj.Victims_status){
+                obj.hasVictims = true;
+            }else{
+                obj.hasVictims = false;
+            }
+
+            console.log(obj.hasVictims);
+
+
+        })
+
+        var hbMainTemplate = Handlebars.compile(mainTemplate);
+        var compiled = hbMainTemplate(sheets);
+        var html = compiled;
+        return html;
+    });
+
+  
+}
+
