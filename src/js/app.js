b8edc8638f2a4d13030e522c677221f1c86bb008
Garry Blight <garry.blight@guardian.co.uk>
2017-10-03 11:35:39 +0100

    Josh style changes

--- src/js/app.js
+++ src/js/app.js
@@ -15,7 +15,7 @@ import listTemplate from '../templates/list.html'
 
 import shares from './share'
 
-let shareFn = shares('Next Generation Footballers 2017 - Rest of the World', 'https://gu.com/p/793ff', '');
+let shareFn = shares('Next Generation 2017: 60 of the best young talents in world football', 'https://gu.com/p/793ff', '');
 
 
 //import gridPicTemplate from '../templates/gridPic.html'
