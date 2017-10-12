8dd57c51d3f960140d3a83789b8cd3f2240ed033
Garry Blight <garry.blight@guardian.co.uk>
2017-10-12 12:33:11 +0100

    z-indexes changed to sub 1000, flags removed from markup

--- src/js/app.js
+++ src/js/app.js
@@ -85,7 +85,7 @@ xr.get(url).then((resp) => {
 
 function cleanData(dataIn) {
 
-    console.log(dataIn);
+    //console.log(dataIn);
 
     var obj, dataOut = {},
         arr;
@@ -309,7 +309,7 @@ function updateLazyLoad() {
 
 
 function updateOnScroll() {
-    console.log("scrolled");
+    //console.log("scrolled");
     //console.log(document.documentElement.scrollTop || document.body.scrollTop);
     checkFixElements();
 }
@@ -456,12 +456,12 @@ function checkFixElements() {
 
         var pos_top = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
 
-        console.log("pos_top=" + pos_top);
-        console.log("oh=" + h);
+        //console.log("pos_top=" + pos_top);
+        //console.log("oh=" + h);
 
 
         if (pos_top > h) {
-            console.log("fixed");
+            //console.log("fixed");
             document.querySelector('#toggle-view-overlay-btn').classList.add('gv-fixed');
             document.querySelector('#toggle-view-overlay-btn').style.marginTop = -h + "px";
         } else if (pos_top < h) {
@@ -563,7 +563,7 @@ function drawPositions(data) {
             el = document.getElementById('gv-pitch_' + i + '_marker_' + id);
             //el = document.querySelector('#list-entry_' + i  );
 
-            console.log(el)
+            //console.log(el)
 
             el.style.visibility = "visible";
 
