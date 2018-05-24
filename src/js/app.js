e8d4e081929637790a5636535a2fc69bd32d55a3
cfen <chris.fenn@guardian.co.uk>
2018-05-24 15:38:28 +0100

    Set up for women's cricket

--- src/js/app.js
+++ src/js/app.js
@@ -60,7 +60,7 @@ function getStyle (element) {
 
 var url;
 //url = 'https://interactive.guim.co.uk/docsdata/1_F-62z-eeeV1mP3OS1SNcF4b8s3deiAx0bxVmDqP98Q.json'; // Old 2016 Next Gen World
-url = 'https://interactive.guim.co.uk/docsdata/1yKh0V2u8VnW1B_MYCHG1ggcTN6a0bl8gDuXmY8LEAtY.json'; // New 2017 Next Gen world
+url = 'https://interactive.guim.co.uk/docsdata-test/1su_inOe_vhoksNrV9VLO9tXEs22pGTDnzJDp-VcS0wg.json'; // Womens Cricket
 
 
 xr.get(url).then((resp) => {
